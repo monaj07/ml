df8cacc5bfc784e8491dcf95292e41c1a33097d3
Monaj <najafi.ece@gmail.com>
2020-08-03 12:15:23 +1000

    Add the uncertainty notebook/data

--- uncertainty/dataset.py
+++ uncertainty/dataset.py
@@ -1,3 +1,6 @@
+import glob
+import os
+import PIL
 from torch.utils import data
 
 
@@ -18,3 +21,20 @@ class CreateDataBatches(data.Dataset):
         if self.normalize:
             input_data = (input_data - self.data_mean) / self.data_std
         return input_data, label
+
+
+class RandomTestDataset(data.Dataset):
+    def __init__(self, path, transform=None):
+        self.path = path
+        self.transform = transform
+        self.image_files = [filename for filename in os.listdir(path)]
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        image_file = self.image_files[idx]
+        image = PIL.Image.open(os.path.join(self.path, image_file))
+        if self.transform:
+            image = self.transform(image)
+        return image, image_file
