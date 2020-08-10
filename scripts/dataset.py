b6d93a6d187163dd40b85865db2315f26553ce94
Monaj <najafi.ece@gmail.com>
2020-08-11 05:00:23 +1000

    Train a many-to-many network with conv1d

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -55,3 +55,30 @@ class TimeSeriesDataset(data.Dataset):
         if self.normalize:
             input_data = (input_data - self.data_mean) / self.data_std
         return input_data, label
+
+
+class TimeSeriesDatasetMature(data.Dataset):
+    def __init__(self, input_data, input_width, label_width, shift, feature_columns, label_columns):
+        window_size = input_width + shift
+        num_samples = input_data.shape[0] - window_size
+        self.regressors = []
+        self.output_labels = []
+        self.input_labels = []
+        label_data = input_data[label_columns]
+        regressors = input_data[feature_columns]
+        for i in range(num_samples):
+            regressor = regressors.iloc[i:i+window_size, :]
+            future_labels = label_data.iloc[i+window_size-label_width:i+window_size]
+            past_labels = label_data.iloc[i:i+input_width]
+            self.regressors.append(regressor.to_numpy())
+            self.output_labels.append(future_labels.to_numpy())
+            self.input_labels.append(past_labels.to_numpy())
+
+    def __len__(self):
+        return len(self.output_labels)
+
+    def __getitem__(self, idx):
+        output_label = self.output_labels[idx]
+        input_label = self.input_labels[idx]
+        regressor = self.regressors[idx]
+        return regressor, input_label, output_label
