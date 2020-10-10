52386fe08b7cd4d98c7f598385d1203b24d46f66
Monaj <najafi.ece@gmail.com>
2020-10-11 00:24:23 +1100

    BugFix: Fix the dimensions for TeacherForcing case

--- nlp/text_generation.py
+++ nlp/text_generation.py
@@ -27,8 +27,8 @@ class RNN(nn.Module):
         return out, (h, c)
 
     def init_weights(self, batch_size):
-        h = torch.zeros(self.num_rnn_layers, batch_size, self.hidden_size)
-        c = torch.zeros(self.num_rnn_layers, batch_size, self.hidden_size)
+        h = torch.zeros(self.num_rnn_layers, batch_size, self.hidden_size).to(device)
+        c = torch.zeros(self.num_rnn_layers, batch_size, self.hidden_size).to(device)
         return h, c
 
 
@@ -50,8 +50,8 @@ class Generation:
         optimizer = optim.Adam(self.rnn.parameters(), lr=lr)
 
         loss_avg = 0
-        self.rnn.train()
-        for it in tqdm(range(iterations)):
+        for it in range(iterations):
+            self.rnn.train()
             optimizer.zero_grad()
             start_idx = np.random.randint(0, len(names) - length - 1)
             data = names[start_idx: start_idx + length + 1]
@@ -60,21 +60,21 @@ class Generation:
             h, c = self.rnn.init_weights(1)
             loss = 0
             for idx in range(length):
-                if idx > 0 and it > 1000 and np.random.choice(1+(iterations - it)//500) == 0:
+                if idx > 0 and it > 100 and np.random.choice(1+(iterations - it)//500) == 0:
                     # Teacher forcing (Thanks to Sadegh Aliakbarian for introducing me to this idea)
                     out_wide = out.data.view(-1).div(self.temperature).exp()
                     out = torch.multinomial(out_wide, 1)
                     in_char = self.char_tensor(self.all_characters[out.item()]).to(device=device)
                 else:
-                    in_char = input[idx]
-                out, (h, c) = self.rnn(in_char.unsqueeze(0), h, c)
+                    in_char = input[idx].unsqueeze(0)
+                out, (h, c) = self.rnn(in_char, h, c)
                 loss += F.cross_entropy(out, output[idx].unsqueeze(0))
             loss.backward()
             optimizer.step()
 
             loss_avg += loss.item()
             if it % 50 == 0 and it > 0:
-                print(f'average loss over the past 50 iterations: {np.round(loss_avg / 50, 2)}')
+                print(f'\nIteration {it} - average loss over the past 50 iterations: {np.round(loss_avg / 50, 2)}')
                 loss_avg = 0
                 print(self.generate())
 
