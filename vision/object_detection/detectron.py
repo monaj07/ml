59d9193ba0624b2fc536ced6e83218faa010efbd
Monaj <najafi.ece@gmail.com>
2020-11-24 22:38:22 +1100

    Add references to YOLOv5 and Detectron2 repos

---
+++ vision/object_detection/detectron.py
@@ -0,0 +1,9 @@
+"""
+https://github.com/facebookresearch/detectron2
+https://gilberttanner.com/blog/detectron-2-object-detection-with-pytorch
+https://colab.research.google.com/drive/16jcaJoc6bCFAQ96jDe2HwtXj7BMD_-m5
+https://towardsdatascience.com/object-detection-in-6-steps-using-detectron2-705b92575578
+https://curiousily.com/posts/face-detection-on-custom-dataset-with-detectron2-in-python/
+https://www.mrdbourke.com/airbnb-amenity-detection/
+https://www.dlology.com/blog/how-to-train-detectron2-with-custom-coco-datasets/
+"""
---
+++ vision/object_detection/yolo_v5.py
@@ -0,0 +1,4 @@
+"""
+https://github.com/monaj07/yolov5/blob/master/yolov5x_finetune.ipynb
+https://curiousily.com/posts/object-detection-on-custom-dataset-with-yolo-v5-using-pytorch-and-python/
+"""
